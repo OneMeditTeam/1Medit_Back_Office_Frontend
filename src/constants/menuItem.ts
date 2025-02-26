7e0ef14ffc98a7aa76cbd6426434bd13d2ad5b1a
ohofront <youngho7482@gmail.com>
2025-02-26 17:33:17 +0900

    feat: :sparkles: (#9): lucide-react 활용 menuItems 상수값 생성

---
+++ src/constants/menuItem.ts
@@ -0,0 +1,17 @@
+import {
+  MessageSquare,
+  LayoutDashboard,
+  Users,
+  Store,
+  BarChart,
+  Inbox,
+} from "lucide-react";
+
+export const menuItems = [
+  { title: "화면 관리", icon: LayoutDashboard },
+  { title: "회원 관리", icon: Users },
+  { title: "입점 관리", icon: Store },
+  { title: "매출 관리", icon: BarChart },
+  { title: "리뷰 관리", icon: MessageSquare },
+  { title: "문의 관리", icon: Inbox },
+];
